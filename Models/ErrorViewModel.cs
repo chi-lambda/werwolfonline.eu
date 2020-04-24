dbc57dd8cd187eecd90f0252fb30c3e6a183f8e7
Chi Lambda <15052875+chi-lambda@users.noreply.github.com>
2020-04-24 09:49:27 +0200

    added .NET Core project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace werwolfonline.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
